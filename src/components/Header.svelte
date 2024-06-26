46605440643a7651dc3b0760391a13e7ed5bc050
TanV-04 <aretanvi@gmail.com>
2024-06-26 17:14:02 +0530

    Ninth Commit

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -14,7 +14,7 @@
 > -->
 
 <header
-  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
+  class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid sm:hidden" +
     (y > 0
       ? " py-4 bg-slate-950 border-violet-450 text-white"
       : " py-6 bg-transparent border-transparent")}
